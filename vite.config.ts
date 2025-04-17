0b4ec1b99d4827bd4f7a5025d164bb680604409e
Javier <javiermignolet@outlook.com>
2025-04-17 16:01:46 -0300

    icono

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001, // 👈 acá configuramos el puerto
+    port: 3000, // 👈 acá configuramos el puerto
   },
   base: "./",
 });
